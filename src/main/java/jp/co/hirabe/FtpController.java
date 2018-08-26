f6d76554a992ade0b9f920e4592209f7088d8d54
Hiroyuki Abe <hirabe@yahoo-corp.jp>
2018-08-26 17:58:38 +0900

    FTP接続サンプル

--- src/main/java/jp/co/hirabe/FtpController.java
+++ src/main/java/jp/co/hirabe/FtpController.java
@@ -1,13 +1,60 @@
 package jp.co.hirabe;
 
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class FtpController {
 
     @RequestMapping("/ftp")
-    public String greeting() {
-        return "hello";
+    public String ftp() {
+        FTPClient ftp = new FTPClient();
+        FTPClientConfig config = new FTPClientConfig();
+        ftp.configure(config);
+        boolean error = false;
+        try {
+            int reply;
+            String server = "localhost";
+            ftp.connect(server, 21);
+            System.out.println("Connected to " + server + ".");
+            System.out.print(ftp.getReplyString());
+
+            // After connection attempt, you should check the reply code to verify
+            // success.
+            reply = ftp.getReplyCode();
+            System.out.println(reply);
+
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+                System.err.println("FTP server refused connection.");
+                System.exit(1);
+            }
+
+            ftp.login("sample", "sample");
+            FTPFile[] list = ftp.listFiles("/test");
+
+            System.out.println(list.length);
+            // transfer files
+            ftp.logout();
+        } catch (IOException e) {
+            error = true;
+            e.printStackTrace();
+        } finally {
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch (IOException ioe) {
+                    // do nothing
+                }
+            }
+            return error ? "1" : "0";
+        }
     }
 }
